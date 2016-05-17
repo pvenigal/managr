bbcac02c1cfcfd8bc8152d9586fca3b27dad8b80
Prasanth Venigalla <prasanthvenigalla@mail.adelphi.edu>
2016-05-17 21:36:35 +0000

    fixed send buton text

--- db/schema.rb
+++ db/schema.rb
@@ -32,13 +32,6 @@ ActiveRecord::Schema.define(version: 20160517144228) do
 
   add_index "entries", ["user_id"], name: "index_entries_on_user_id"
 
-  create_table "news", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
